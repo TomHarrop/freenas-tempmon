9896a45f1aa0c94cea99f6b3365854328be0d1ff
Tom Harrop <twharrop@gmail.com>
2016-07-24 00:34:27 +0200

    plot

--- src/draw_graph.R
+++ src/draw_graph.R
@@ -1,27 +1,39 @@
+#!/usr/bin/env Rscript
+
 library(ggplot2)
-library(data.table)
+library(dplyr)
 
-# load data
-col.names <- c("time", "sensor", "serial", "temperature")
-colClasses <- c("numeric", "character", "character", "character")
-sensor.data <- data.table(read.csv("dat/sample.csv", header = FALSE, col.names = col.names,
-                                   colClasses = colClasses, na.strings = c("", "NA"),
-                                   fill = TRUE, stringsAsFactors = FALSE))
+# data load function
+LoadSensorData <- function(sensor.file) {
+    col.names <- c("time", "sensor", "serial", "temperature")
+    colClasses <- c("numeric", "character", "character", "character")
+    sensor.data.frame <- read.csv(sensor.file, header = FALSE,
+        col.names = col.names, colClasses = colClasses,
+        na.strings = c("", "NA"), fill = TRUE, stringsAsFactors = FALSE)
+}
 
-# convert epoch time to POSIXct
+# load data for each sensor
+sensors <- c(weather = "dat/weather.csv", disk = "dat/disk_temp.csv",
+    cpu = "dat/cpu_temp.csv")
+sensor.data <- dplyr::bind_rows(lapply(sensors, LoadSensorData), .id = "type")
+
+# convert epoch time to POSIXct and convert temp to numeric
 origin <- "1970-01-01"
-sensor.data[, time := as.POSIXct(time, origin = origin)]
 
-# fix temperature readings
-sensor.data[, temperature := as.numeric(sub("C", "", temperature))]
+plot.data <- sensor.data %>% mutate(
+    time = as.POSIXct(time, origin = origin),
+    temperature = as.numeric(sub("C", "", temperature))) %>%
+    filter(!is.na(temperature))
 
-# split disk vs cpu
-sensor.data[grep("^cpu", sensor), type := "cpu"]
-sensor.data[is.na(type), type := "disk"]
-
-# plot
-ggplot(sensor.data[!is.na(temperature)], aes(x = time, y = temperature, group = sensor, colour = sensor)) +
-#  facet_wrap(~type, ncol = 1) +
-  scale_color_discrete() +
-  geom_smooth(method = "glm", se = FALSE, size = 0.5, alpha = 0.7) +
-  geom_point(size = 3, alpha = 0.7)
+# save plot
+if (!dir.exists("out")) {
+    dir.create("out")
+}
+pdf("out/graph.pdf")
+ggplot(plot.data,
+    aes(x = time, y = temperature, group = sensor, colour = sensor)) +
+    theme_minimal() +
+    scale_color_discrete() +
+    geom_smooth(method = "glm", se = FALSE, size = 0.5, alpha = 0.7) +
+    geom_point(size = 3, alpha = 0.7)
+dev.off()
