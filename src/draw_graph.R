024c382ba3b8d2a22f85b16fa575732885f6ec3a
Tom Harrop <twharrop@gmail.com>
2016-07-24 16:59:59 +0200

    graph with facets

--- src/draw_graph.R
+++ src/draw_graph.R
@@ -25,29 +25,45 @@ plot.data <- sensor.data %>% mutate(
   temperature = as.numeric(sub("C", "", temperature)),
   sensor = relevel(factor(sensor), ref = unique(sensor[type == "weather"]))) %>%
   filter(!is.na(temperature))
+plot.data <- plot.data %>% mutate(sensor = factor(sensor))
+
+# make weather data for superimposing
+weather.data <- filter(plot.data, type == "weather")
+MutateWeather <- function(sensor.name){
+  mutate(weather.data, sensor = sensor.name)
+}
+mutated.list <- lapply(as.character(unique(filter(
+  plot.data, !type == "weather")$sensor)), MutateWeather)
+weather.plot <- dplyr::bind_rows(mutated.list)
+
+# set colours
+n.disk <- length(unique(plot.data$sensor[plot.data$type == "disk"]))
+n.cpu <- length(unique(plot.data$sensor[plot.data$type == "cpu"]))
+break.labels <- levels(plot.data$sensor)
+
+c1 <- "#e41a1c"
+c2 <- tail(RColorBrewer::brewer.pal(9, "Blues"), n.disk)
+c3 <- tail(RColorBrewer::brewer.pal(9, "Greens"), n.cpu)
+palette <- c(c1, c2, c3)
 
 # save plot
 if (!dir.exists("out")) {
   dir.create("out")
 }
-pdf("out/graph.pdf")
 
-n.disk <- length(unique(plot.data$sensor[plot.data$type == "disk"]))
-n.cpu <- length(unique(plot.data$sensor[plot.data$type == "cpu"]))
-
-c1 <- "#e41a1c"
-c2 <- tail(RColorBrewer::brewer.pal(9, "Blues"), n.disk)
-c3 <- tail(RColorBrewer::brewer.pal(9, "Greens"), n.cpu)
-
-palette <- c(c1, c2, c3)
-
-ggplot(plot.data[!plot.data$type == "weather",],
-       aes(x = time, y = temperature, group = sensor, colour = sensor)) +
+pdf("out/graph.pdf", width = 10, height = 7.5)
+ggplot(filter(plot.data, !type == "weather"),
+             aes(x = time, y = temperature, group = sensor, colour = sensor)) +
   facet_wrap(~sensor) +
   theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1),
+        axis.ticks.length = unit(0, "mm")) +
   xlab("Time") + ylab("Temperature (°C)") +
-  scale_color_manual(values = palette, guide = guide_legend(title = NULL)) +
+  scale_color_manual(values = palette, breaks = break.labels,
+                     labels = break.labels, drop = FALSE,
+                     guide = guide_legend(title = NULL),
+                     expand = c(0, 0)) +
   geom_line() +
-  geom_line(aes(colour = c1), data = plot.data[plot.data$type == "weather",])
+  geom_line(mapping = aes(colour = type), data = weather.plot, colour = c1)
 
 dev.off()
